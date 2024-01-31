41c454d4344a0937456bd910d03153632a208d5f
shturkhal <107053994+shturkhal@users.noreply.github.com>
2024-01-31 17:52:28 +0200

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Іван Штурхаль on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
