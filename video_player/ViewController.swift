d7e43031cf257c077e1042a1cf18b43d044fa137
shturkhal <107053994+shturkhal@users.noreply.github.com>
2024-01-31 19:36:41 +0200

    add some comments

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -21,14 +21,19 @@ class ViewController: UIViewController {
         playVideo()
     }
     private func playVideo() {
+        // створити шлях до відео
         guard let path = Bundle.main.path(forResource: "test", ofType: "mp4") else {
             debugPrint("test.mp4 not found")
             
             return
         }
+        // створити плеєр з позначенням шляху до відео
         let player = AVPlayer(url: URL(fileURLWithPath: path))
+        // створити вікно плеєра
         let playerController = AVPlayerViewController()
+        // позначити у вікні плеєра плеєр з щляхом до відео
         playerController.player = player
+        // презентувати на екран вікно плеєру, та запустити його
         present(playerController, animated: true) {
             player.play()
         }
