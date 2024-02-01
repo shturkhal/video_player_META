38e3e0a43413fe7def0e18e3f99b3989db081941
shturkhal <107053994+shturkhal@users.noreply.github.com>
2024-02-01 13:37:59 +0200

    done with terminal

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -10,36 +10,30 @@ import AVKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         playVideo()
     }
-    private func playVideo() {
-        // створити шлях до відео
-        guard let path = Bundle.main.path(forResource: "test", ofType: "mp4") else {
+    private func playVideo(){
+        guard let path=Bundle.main.path(forResource: "test", ofType:"mp4") else {
             debugPrint("test.mp4 not found")
-            
             return
         }
-        // ініциалізувати плеєр з позначенням шляху до відео
         let player = AVPlayer(url: URL(fileURLWithPath: path))
-        // створити вікно плеєра
         let playerController = AVPlayerViewController()
-        // позначити у вікні плеєра плеєр з щляхом до відео
         playerController.player = player
-        // презентувати на екран вікно плеєру, та запустити його
+        
         present(playerController, animated: true) {
             player.play()
+            
         }
+        
     }
-    
-   
-    
 }
 
